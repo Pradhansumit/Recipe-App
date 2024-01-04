8291084de2e50c88379fd6f42e4e3ec3fa38c189
Pradhansumit <pradhansumit026@gmail.com>
2024-01-04 08:17:38 +0530

    added styling, added modal

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ const Food = [
     ingredients: "Chicken, Tumeric, Yoghurt, Onion, Tamatoes",
     tags: "Dinner",
     image: "public/images/chicken-masala.jpg",
+    description:
+      "Chicken masala is a simple Indian dish made with Chicken, spices, herbs, onions and tomatoes. Serve this with rice or any flatbreads like naan, roti or paratha.",
   },
   {
     id: 2,
@@ -12,6 +14,8 @@ const Food = [
     ingredients: "Fish, Tumeric, Onion, Tamatoes",
     tags: "Lunch",
     image: "public/images/fish-curry.jpg",
+    description:
+      "Simple fish curry made in INdian style. Mildly spicy, flavorful and delicious tasting fish curry. Serve it with rice or roti.",
   },
   {
     id: 3,
@@ -19,6 +23,8 @@ const Food = [
     ingredients: "Egg, Coriander",
     tags: "Breakfast",
     image: "public/images/omlette.jpg",
+    description:
+      "An omelet is a dish you might order for breakfast or brunch — it's kind of like a folded pancake made of beaten eggs, sometimes with the addition of vegetables, cheese, or meat. Yum. A good omelet is a little bit fluffy, but firm enough to contain its filling",
   },
   {
     id: 4,
@@ -26,6 +32,8 @@ const Food = [
     ingredients: "Dosa, Mashed Potato, Chutney",
     tags: "Brunch",
     image: "public/images/masala-dosa.jpg",
+    description:
+      "Dosa is a crepe made using fermented rice and lentil batter. Masala Dosa is one that is crisp, aromatic, flavorful and has a potato masala or spiced seasoned potatoes stuffed in it.",
   },
 ];
 
@@ -33,6 +41,8 @@ import { useState } from "react";
 
 function App() {
   const [itemList, setItemList] = useState(Food);
+  const [modalVisible, setModalVisible] = useState(null);
+  const [selectedItem, setSelectedItem] = useState([]);
 
   function handleSearch(e) {
     console.log(e);
@@ -43,13 +53,31 @@ function App() {
     );
   }
 
+  function setModalOn(id) {
+    setModalVisible(1);
+    setSelectedItem(itemList.filter((item) => item.id === id));
+    console.log(selectedItem);
+  }
+  function setModalOff() {
+    setModalVisible(null);
+  }
+
   return (
-    <div className="App">
-      <Navigation />
-      <SearchBar handleSearch={handleSearch} />
-      <FilterOptions setItemList={setItemList} />
-      <Menu itemList={itemList} />
-    </div>
+    <>
+      <div className={`App ${modalVisible ? "blur" : ""}`}>
+        <Navigation />
+        <SearchBar handleSearch={handleSearch} />
+        <FilterOptions setItemList={setItemList} />
+        <Menu itemList={itemList} setModalOn={setModalOn} />
+      </div>
+      <div>
+        <Modal
+          setModalOff={setModalOff}
+          modalVisible={modalVisible}
+          selectedItem={selectedItem}
+        />
+      </div>
+    </>
   );
 }
 
@@ -136,16 +164,18 @@ function FilterOptions({ setItemList }) {
   );
 }
 
-function Menu({ itemList }) {
+function Menu({ itemList, setModalOn }) {
   return (
     <div className="Menu-Container">
       <div className="Menu">
         {itemList.map((fooditem) => (
           <Item
             key={fooditem.id}
+            id={fooditem.id}
             desc={fooditem.ingredients}
             image={fooditem.image}
             name={fooditem.title}
+            setModalOn={setModalOn}
           />
         ))}
       </div>
@@ -153,12 +183,43 @@ function Menu({ itemList }) {
   );
 }
 
-function Item({ desc, image, name }) {
+function Item({ id, desc, image, name, setModalOn }) {
+  function handleClick() {
+    setModalOn(id);
+  }
+
   return (
     <div className="item">
       <h3>{name}</h3>
       <img className="item-image" src={image} alt={name} />
       <p>{desc}</p>
+      <button onClick={handleClick} className="know-more">
+        Know More
+      </button>
     </div>
+  );
+}
+
+function Modal({ setModalOff, modalVisible, selectedItem }) {
+  function handleClick() {
+    setModalOff();
+  }
+  return (
+    modalVisible && (
+      <div className="Modal">
+        <div className="Modal-body">
+          <button onClick={handleClick} className="modal-close">
+            &times;
+          </button>
+          <h2>{selectedItem[0].title}'s Recipe</h2>
+          <ul>
+            <h4>Ingredients:</h4>
+            <p>{selectedItem[0].ingredients}</p>
+            <h4>Description:</h4>
+            <p>{selectedItem[0].description}</p>
+          </ul>
+        </div>
+      </div>
+    )
   );
 }
