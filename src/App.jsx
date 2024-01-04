7f39b312bb7e60eff25a5fdaed8d8cb1fb1d8d45
Pradhansumit <pradhansumit026@gmail.com>
2024-01-04 08:26:25 +0530

    another file for data

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,5 @@
-const Food = [
-  {
-    id: 1,
-    title: "Chicken Masala",
-    ingredients: "Chicken, Tumeric, Yoghurt, Onion, Tamatoes",
-    tags: "Dinner",
-    image: "public/images/chicken-masala.jpg",
-    description:
-      "Chicken masala is a simple Indian dish made with Chicken, spices, herbs, onions and tomatoes. Serve this with rice or any flatbreads like naan, roti or paratha.",
-  },
-  {
-    id: 2,
-    title: "Fish Curry",
-    ingredients: "Fish, Tumeric, Onion, Tamatoes",
-    tags: "Lunch",
-    image: "public/images/fish-curry.jpg",
-    description:
-      "Simple fish curry made in INdian style. Mildly spicy, flavorful and delicious tasting fish curry. Serve it with rice or roti.",
-  },
-  {
-    id: 3,
-    title: "Omlette",
-    ingredients: "Egg, Coriander",
-    tags: "Breakfast",
-    image: "public/images/omlette.jpg",
-    description:
-      "An omelet is a dish you might order for breakfast or brunch — it's kind of like a folded pancake made of beaten eggs, sometimes with the addition of vegetables, cheese, or meat. Yum. A good omelet is a little bit fluffy, but firm enough to contain its filling",
-  },
-  {
-    id: 4,
-    title: "Masala Dosa",
-    ingredients: "Dosa, Mashed Potato, Chutney",
-    tags: "Brunch",
-    image: "public/images/masala-dosa.jpg",
-    description:
-      "Dosa is a crepe made using fermented rice and lentil batter. Masala Dosa is one that is crisp, aromatic, flavorful and has a potato masala or spiced seasoned potatoes stuffed in it.",
-  },
-];
-
 import { useState } from "react";
+import { Food } from "./Food";
 
 function App() {
   const [itemList, setItemList] = useState(Food);
